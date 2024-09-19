84faf38fd27a3b1dbfdb2d4c5552e041cc6f0727
ryansanchez2027 <ryansanchez@westminster.net>
2024-09-19 18:05:25 -0400

    ahhhhh

--- src/full_featured.ino
+++ src/full_featured.ino
@@ -1,72 +1,220 @@
-#include "ArducamLink.h"
-#include "Arducam_Mega.h"
-#include "SPI.h"
+/*
+  WiFi Web Server LED Blink
 
-const int MEGA_CS = 7;
-Arducam_Mega myCAM(MEGA_CS);
-ArducamLink myUart;
+  A simple web server that lets you blink an LED via the web.
+  This sketch will print the IP address of your WiFi module (once connected)
+  to the Serial Monitor. From there, you can open that address in a web browser
+  to turn on and off the LED_BUILTIN.
 
-unsigned long lastCaptureTime = 0;
-const unsigned long captureInterval = 10000; // 10 seconds in milliseconds
+  If the IP address of your board is yourAddress:
+  http://yourAddress/H turns the LED on
+  http://yourAddress/L turns it off
 
-// Function declarations
-void captureAndSendImage();
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
 
-void setup()
-{
-    Serial.begin(115200);  // Initialize serial for debugging
-    SPI.begin();
-    myUart.arducamUartBegin(115200);
-    myUart.send_data_pack(7, "Hello Arduino UNO!");
-    myCAM.begin();
-    myUart.send_data_pack(8, "Mega start!");
-    Serial.println("Setup complete");
+  Circuit:
+  * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and Uno WiFi Rev.2)
+  * LED attached to pin 9
+
+  created 25 Nov 2012
+  by Tom Igoe
+
+  Find the full UNO R4 WiFi Network documentation here:
+  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#simple-webserver
+ */
+
+#include "WiFiS3.h"
+
+#include "arduino_secrets.h"               // your network key index number (needed only for WEP)
+
+int led =  LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);      // initialize serial communication
+  pinMode(led, OUTPUT);      // set the LED pin mode
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();                           // start the web server on port 80
+  printWifiStatus();                        // you're connected now, so print out the status
 }
 
-void loop()
-{
-    unsigned long currentTime = millis();
 
-    if (currentTime - lastCaptureTime >= captureInterval) {
-        captureAndSendImage();
-        lastCaptureTime = currentTime;
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out to the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
+            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
+            
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+      }
+      
     }
-    myCAM.captureThread();
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
 
-void captureAndSendImage()
-{
-    myCAM.takePicture(CAM_IMAGE_MODE_UXGA, CAM_IMAGE_PIX_FMT_JPG);
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
 
-    // Send start marker
-    myUart.arducamUartWrite(0xFF);
-    myUart.arducamUartWrite(0xAA);
-    myUart.arducamUartWrite(0x01);
-    Serial.println("Start marker sent");
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 
-    // Send image size
-    uint32_t imageSize = myCAM.getTotalLength();
-    myUart.arducamUartWrite((uint8_t)(imageSize & 0xFF));
-    myUart.arducamUartWrite((uint8_t)((imageSize >> 8) & 0xFF));
-    myUart.arducamUartWrite((uint8_t)((imageSize >> 16) & 0xFF));
-    myUart.arducamUartWrite((uint8_t)((imageSize >> 24) & 0xFF));
-    Serial.print("Image size: ");
-    Serial.println(imageSize);
-
-    // Send image data
-    uint8_t buffer[256];
-    while (imageSize > 0) {
-        uint16_t bytesToRead = min(256, imageSize);
-        myCAM.readBuff(buffer, bytesToRead);
-        myUart.arducamUartWriteBuff(buffer, bytesToRead);
-        imageSize -= bytesToRead;
-    }
-
-    // Send end marker
-    myUart.arducamUartWrite(0xFF);
-    myUart.arducamUartWrite(0xBB);
-    Serial.println("End marker sent");
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
+
+
+
+
+
+// #include "WiFiS3.h"
+// #include <WiFi.h>
+
+// #include "Arducam_Mega.h"
+// #include "SPI.h"
+// #include "ArducamLink.h"
+// #include "arduino_secrets.h"
+
+// const int MEGA_CS = 7;
+// Arducam_Mega myCAM(MEGA_CS);
+// ArducamLink myUart;
+
+// #define SECRET_SSID "ribbit"
+// #define SECRET_PASS "bf3006159a3b2d6b3a16c63ac4"
+
+// unsigned long lastCaptureTime = 0;
+// const unsigned long captureInterval = 10000; // 10 seconds in milliseconds
+
+// // Function declarations
+// void captureAndSendImage();
+
+// void setup()
+// {
+//     Serial.begin(115200);  // Initialize serial for debugging
+//     SPI.begin();
+//     myUart.arducamUartBegin(115200);
+//     myUart.send_data_pack(7, "Hello Arduino UNO!");
+//     myCAM.begin();
+//     myUart.send_data_pack(8, "Mega start!");
+//     Serial.println("Setup complete");
+// }
+
+// void loop()
+// {
+//     unsigned long currentTime = millis();
+
+//     if (currentTime - lastCaptureTime >= captureInterval) {
+//         captureAndSendImage();
+//         lastCaptureTime = currentTime;
+//     }
+//     myCAM.captureThread();
+// }
+
+// void captureAndSendImage()
+// {
+//     myCAM.takePicture(CAM_IMAGE_MODE_UXGA, CAM_IMAGE_PIX_FMT_JPG);
+
+//     // Send start marker
+//     myUart.arducamUartWrite(0xFF);
+//     myUart.arducamUartWrite(0xAA);
+//     myUart.arducamUartWrite(0x01);
+//     Serial.println("Start marker sent");
+
+//     // Send image size
+//     uint32_t imageSize = myCAM.getTotalLength();
+//     myUart.arducamUartWrite((uint8_t)(imageSize & 0xFF));
+//     myUart.arducamUartWrite((uint8_t)((imageSize >> 8) & 0xFF));
+//     myUart.arducamUartWrite((uint8_t)((imageSize >> 16) & 0xFF));
+//     myUart.arducamUartWrite((uint8_t)((imageSize >> 24) & 0xFF));
+//     Serial.print("Image size: ");
+//     Serial.println(imageSize);
+
+//     // Send image data
+//     uint8_t buffer[256];
+//     while (imageSize > 0) {
+//         uint16_t bytesToRead = min(256, imageSize);
+//         myCAM.readBuff(buffer, bytesToRead);
+//         myUart.arducamUartWriteBuff(buffer, bytesToRead);
+//         imageSize -= bytesToRead;
+//     }
+
+//     // Send end marker
+//     myUart.arducamUartWrite(0xFF);
+//     myUart.arducamUartWrite(0xBB);
+//     Serial.println("End marker sent");
+// }
 
 
 
